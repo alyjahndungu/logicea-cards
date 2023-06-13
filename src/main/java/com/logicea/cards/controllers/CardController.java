6100a8bff3809815e279c7d3730f89ee0233c8db
elijah.ndungu <elijahndungu30@gmail.com>
2023-06-13 12:57:19 +0300

    chore: adding more api documentation operations

--- src/main/java/com/logicea/cards/controllers/CardController.java
+++ src/main/java/com/logicea/cards/controllers/CardController.java
@@ -6,6 +6,7 @@ import com.logicea.cards.domain.entities.Users;
 import com.logicea.cards.handlers.ResponseHandler;
 import com.logicea.cards.services.CardService;
 import com.logicea.cards.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -14,7 +15,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.SortDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -27,6 +27,7 @@ public class CardController {
     private final UserService userService;
     private final CardService cardService;
 
+    @Operation(summary = "Endpoint to  add new cards")
     @PostMapping
     public ResponseEntity<Object> createCards(Principal principal, @Valid @RequestBody CardDto cardDto) {
         Users user = userService.getUser(principal);
@@ -34,6 +35,7 @@ public class CardController {
         return ResponseHandler.generateResponse("Cards created success", HttpStatus.CREATED, cards);
     }
 
+    @Operation(summary = "Endpoint to fetching all cards related to  an individual user")
     @GetMapping
     public ResponseEntity<Object> getCardsForSingleUser(Principal principal,  @SortDefault.SortDefaults({@SortDefault(sort = "id", direction = Sort.Direction.DESC)}) Pageable pageable) {
         Users user = userService.getUser(principal);
@@ -41,23 +43,29 @@ public class CardController {
         return ResponseHandler.generateResponse("Cards fetched success", HttpStatus.OK, cards);
     }
 
+    @Operation(summary = "Endpoint to search for cards")
     @GetMapping("search/{searchName}")
     public ResponseEntity<Object> searchCards(@PathVariable String searchName) {
         List<Cards> cards = cardService.searchCards(searchName);
         return ResponseHandler.generateResponse("Cards fetched successfully", HttpStatus.OK, cards);
     }
 
+    @Operation(summary = "Endpoint to update card details")
     @PatchMapping (value = "{id}")
     public ResponseEntity<Object> updateCards(@PathVariable Long id, @Valid @RequestBody CardDto cardDto) {
         Cards cards = cardService.updateCards(id, cardDto);
         return ResponseHandler.generateResponse("Cards updated successfully", HttpStatus.ACCEPTED, cards);
     }
+
+    @Operation(summary = "Endpoint for ADMIN to view all cards")
     @GetMapping(value = "admin")
-    public ResponseEntity<Object> adminGetAllCards(@SortDefault.SortDefaults({@SortDefault(sort = "id", direction = Sort.Direction.DESC)}) Pageable pageable) {
+    public ResponseEntity<Object> adminGetAllCards(@SortDefault.SortDefaults({@SortDefault(sort = "name", direction = Sort.Direction.DESC)}) Pageable pageable,   @RequestParam(defaultValue = "0") int page,
+                                                   @RequestParam(defaultValue = "20") int size) {
         Page<Cards> cards = cardService.adminGetAllCards(pageable);
         return ResponseHandler.generateResponse("Cards fetched success", HttpStatus.OK, cards);
     }
 
+    @Operation(summary = "Endpoint to delete cards")
     @DeleteMapping(value = "{id}")
     public  ResponseEntity<Object> deleteCard(@PathVariable Long id){
         cardService.deleteCard (id);
--- src/main/java/com/logicea/cards/controllers/UserController.java
+++ src/main/java/com/logicea/cards/controllers/UserController.java
@@ -6,6 +6,7 @@ import com.logicea.cards.domain.entities.Users;
 import com.logicea.cards.domain.models.AuthResponse;
 import com.logicea.cards.handlers.ResponseHandler;
 import com.logicea.cards.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,12 +22,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
 
+    @Operation(summary = "Endpoint for adding new users")
     @PostMapping
     public ResponseEntity<Object> addUser(@RequestBody UserDto userDto) {
         Users user = userService.addUsers(userDto);
         return ResponseHandler.generateResponse("User Added Successfully", HttpStatus.CREATED, user);
     }
 
+    @Operation(summary = "Endpoint to authenticate user by username and password")
     @PostMapping(value = "/login")
     public ResponseEntity<Object> loginWithUsernameAndPassword(@RequestBody LoginDto loginDto) {
         AuthResponse authResponse = userService.loginWithUsernameAndPassword(loginDto);
